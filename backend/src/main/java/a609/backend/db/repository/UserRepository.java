cb3fb8f23070347adad348bdf4e3543f4c6d7a3f
김지건 <jigun21@naver.com>
2022-07-14 15:41:09 +0900

    Feat: "엔티티 생성 및 테스트" #S07P11A609-25

--- backend/src/main/java/a609/backend/db/repository/UserRepository.java
+++ backend/src/main/java/a609/backend/db/repository/UserRepository.java
@@ -1,4 +1,13 @@
 package a609.backend.db.repository;
 
-public class UserRepository {
+import a609.backend.db.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+    User save(User user);
+    User findOneById(String id);
+    void deleteById(String id);
+    int countById(String id);
 }
