435fc1a014c75196983f3cc973f209594e8d0930
김지건 <jigun21@naver.com>
2022-07-14 19:42:53 +0900

    Feat: "메일발송 기능" #S07P11A609-43

--- backend/src/main/java/a609/backend/service/UserServiceImpl.java
+++ backend/src/main/java/a609/backend/service/UserServiceImpl.java
@@ -2,13 +2,21 @@ package a609.backend.service;
 
 import a609.backend.db.entity.User;
 import a609.backend.db.repository.UserRepository;
+import a609.backend.util.MailUtil;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Random;
 
 @Service
 public class UserServiceImpl implements UserService {
     @Autowired
     UserRepository userRepository;
+
+    @Autowired
+    MailUtil mailUtil;
+
 
     @Override
     public User registUser(User user) {
@@ -45,4 +53,12 @@ public class UserServiceImpl implements UserService {
 //    public User login(User user) {
 //        return userRepository.login(user);
 //    }
+    @Override
+    public void findPassword(String id) {
+        Random random = new Random();
+        String newPassword = RandomStringUtils.randomAlphanumeric(10);
+        // 동시에 db의 패스워드를 업데이트 해줘야함(+암호화)
+        mailUtil.findPassword(id, newPassword);
+        return;
+    }
 }
