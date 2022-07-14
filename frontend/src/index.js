b030f99317dcfdf9f89d2f3533f22f2da47bfdd4
j1ddong <hju191228@gmail.com>
2022-07-14 15:02:36 +0900

    Feat: "라우터 추가" #S07P11A609-41

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import Login from './component/Login'
+import Signup from './component/Signup'
 // import './index.css';
 // import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="login" element={<Login />} />
+      <Route path="signup" element={<Signup />} />
+    </Routes>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
