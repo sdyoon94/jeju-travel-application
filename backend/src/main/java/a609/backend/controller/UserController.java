8094f59436f4f5c449d6ebbf679a4ad38c0437d2
sdyoon94 <sdyoon94@gmail.com>
2022-07-15 13:54:58 +0900

    Feat: "내 프로필 조회" #S07P11A609-40

--- backend/src/main/java/a609/backend/controller/UserController.java
+++ backend/src/main/java/a609/backend/controller/UserController.java
@@ -4,6 +4,7 @@ import a609.backend.db.entity.User;
 import a609.backend.service.UserService;
 import a609.backend.util.EncryptUtil;
 import a609.backend.util.JwtUtil;
+import io.jsonwebtoken.Claims;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -25,18 +26,19 @@ public class UserController {
 
 
     @PostMapping("/users")
-    public ResponseEntity<Map<String, String>> registerUser(@RequestBody User user){
+    public ResponseEntity<Map<String, String>> registerUser(@RequestBody User user) {
         HashMap<String, String> map = new HashMap<>();
-        try{
+        try {
             userService.registerUser(user);
             map.put("message", "Success");
             return new ResponseEntity<>(map, HttpStatus.OK);
-        }catch(Exception e){
+        } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
     }
+
     @GetMapping("/users/confirm/{authKey}")
-    public void confirmUser(@PathVariable String authKey){
+    public void confirmUser(@PathVariable String authKey) {
         System.out.println("HI");
         userService.confirmUser(authKey);
         //링크 누르면 blank, 새 탭으로 열어야 반응함 이유 파악 필요
@@ -45,7 +47,7 @@ public class UserController {
     }
 
     @GetMapping("/users/{userId}")
-    public ResponseEntity<Map<String,String>> countById(@PathVariable String userId) {
+    public ResponseEntity<Map<String, String>> countById(@PathVariable String userId) {
         int count = userService.idCheck(userId);
         if (count == 1) {
             HashMap<String, String> map = new HashMap<>();
@@ -54,8 +56,9 @@ public class UserController {
         }
         return null;
     }
+
     @DeleteMapping("/users/{userId}")
-    public ResponseEntity<Map<String, String>> deleteUser(@PathVariable String userId){
+    public ResponseEntity<Map<String, String>> deleteUser(@PathVariable String userId) {
         HashMap<String, String> map = new HashMap<>();
         userService.deleteUser(userId);
         map.put("message", "Success");
@@ -63,12 +66,12 @@ public class UserController {
     }
 
     @GetMapping("/users/findpwd/{userId}")
-    public ResponseEntity<Map<String, String>> findPassword(@PathVariable String userId){
+    public ResponseEntity<Map<String, String>> findPassword(@PathVariable String userId) {
         HashMap<String, String> map = new HashMap<>();
 
-        if(userService.idCheck(userId)==0){
+        if (userService.idCheck(userId) == 0) {
             map.put("message", "가입되지 않은 이메일입니다.");
-        }else{
+        } else {
             userService.findPassword(userId);
             map.put("message", "임시 비밀번호를 발송하였습니다.");
         }
@@ -76,7 +79,7 @@ public class UserController {
     }
 
     @PatchMapping("/users/{userId}")
-    public ResponseEntity updateUser(@PathVariable String userId, @RequestBody User user){
+    public ResponseEntity updateUser(@PathVariable String userId, @RequestBody User user) {
         userService.updateUser(userId, user);
         HashMap<String, String> map = new HashMap<>();
         map.put("message", "Success");
@@ -84,22 +87,36 @@ public class UserController {
     }
 
     @PostMapping("/auth/login")
-    public ResponseEntity<Map<String,String>> login(@RequestBody User user){
+    public ResponseEntity<Map<String, String>> login(@RequestBody User user) {
         Map<String, String> resultMap = new HashMap<>();
         HttpStatus status = null;
         String loginResult = userService.login(user);
-        if (loginResult.equals("404")){
+        if (loginResult.equals("404")) {
             status = HttpStatus.NOT_FOUND;
-            resultMap.put("message","존재하지 않는 계정입니다.");
+            resultMap.put("message", "존재하지 않는 계정입니다.");
         } else if (loginResult.equals("401")) {
             status = HttpStatus.UNAUTHORIZED;
-            resultMap.put("message","잘못된 비밀번호입니다.");
-        }else{
+            resultMap.put("message", "잘못된 비밀번호입니다.");
+        } else {
             status = HttpStatus.OK;
-            resultMap.put("token",loginResult);
+            resultMap.put("token", loginResult);
         }
 
         return new ResponseEntity<Map<String, String>>(resultMap, status);
     }
 
+    @GetMapping("/users/me")
+    public ResponseEntity<Map<String, Object>> myProfile(@RequestBody String token) {
+        Map<String, Object> resultMap = new HashMap<>();
+        Claims claims = jwtUtil.parseJwtToken(token);
+        HttpStatus status = null;
+        if (claims == null) {
+            resultMap.put("message", "토큰 에러");
+            status = HttpStatus.BAD_REQUEST;
+        } else {
+            resultMap = claims;
+            status = HttpStatus.OK;
+        }
+        return new ResponseEntity<Map<String, Object>>(resultMap, status);
+    }
 }
