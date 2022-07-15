9ed320acc2863589a9e0766472c16bd4c3252c8e
김지건 <jigun21@naver.com>
2022-07-15 11:19:42 +0900

    Feat: "회원정보수정" #S07P11A609-36

--- backend/src/main/java/a609/backend/service/UserService.java
+++ backend/src/main/java/a609/backend/service/UserService.java
@@ -3,10 +3,10 @@ package a609.backend.service;
 import a609.backend.db.entity.User;
 
 public interface UserService {
-    User registUser(User user);
+    User registerUser(User user);
     User searchById(String id);//회원조회
     void deleteUser(String id);
-//    void updateUser(User user);//회원 정보 수정
+    void updateUser(String id, User user);//회원 정보 수정
     int idCheck(String id);
     User showInfo(String id);//내 프로필 조회
   //  User login(User user);//로그인
