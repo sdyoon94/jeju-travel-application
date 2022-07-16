a8ffa5b38ce5e9aac9d31818df49d5f45a263db0
j1ddong <hju191228@gmail.com>
2022-07-16 18:24:01 +0900

    Chore: "리덕스 툴키트 초기 설정"
    auth.js 안에 있는 코드는 작동하는 코드가 아니라 예시코드입니다

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
-import store from './store'
+import store from './store/store'
 import App from './App';
 import Login from './routes/Login'
 import Signup from './routes/Signup'
---
+++ frontend/src/store/store.js
@@ -0,0 +1,12 @@
+import { configureStore, combineReducers } from "@reduxjs/toolkit"
+import auth from './modules/auth'
+
+const rootReducer = combineReducers({
+    auth
+})
+
+
+
+export default configureStore({
+    reducer: rootReducer,
+})
