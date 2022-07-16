223e19f85360d0cf6c497f17bfa3d1c6592923e7
j1ddong <hju191228@gmail.com>
2022-07-16 16:41:18 +0900

    Feat: "로그인 페이지 구현" #S07P11A609-28
    사용자가 입력한 값 저장

--- frontend/src/routes/Login.js
+++ frontend/src/routes/Login.js
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import { Link } from "react-router-dom"
 import "../globalStyle.css"
 import "./Login.css"
@@ -5,17 +6,37 @@ import "./Login.css"
 
 
 function Login() {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [loginStay, setLoginStay] = useState(true)
+
+  const handleEmail = function(e) {
+    setEmail(e.target.value)
+  }
+
+  const handlePassword = function(e) {
+    setPassword(e.target.value)
+  }
+
+  const handleLoginStay = function() {
+    setLoginStay(!loginStay)
+  }
+
+  const handleLogin = function(e) {
+    e.preventDefault()
+  }
+
   return(
     <div className="center">
       <h2 className="login-top">이메일로 로그인</h2>
-      <form>
+      <form onSubmit={handleLogin}>
         <label htmlFor="email" className="block">이메일</label>
-        <input id="email" type="email" className="input-auth" />
+        <input id="email" type="email" className="input-auth" value={email} onChange={handleEmail} />
         <label htmlFor="password" className="block">비밀번호</label>
-        <input id="password" type="password" className="input-auth" />
+        <input id="password" type="password" className="input-auth" value={password} onChange={handlePassword} />
         <div>
-        <input type="checkbox" id="onLogin" className="input-checkbox"/>
-        <label htmlFor="onLogin">로그인 유지하기</label>
+          <input type="checkbox" id="onLogin" className="input-checkbox" checked={loginStay} onChange={handleLoginStay}/>
+          <label htmlFor="onLogin">로그인 유지하기</label>
         </div>
         <button className="auth-btn">로그인</button>
       </form>
