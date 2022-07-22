d69b72c2f028fc63b311107a039c0cb9c9d8b738
양소정 <thwjd793@naver.com>
2022-07-22 14:32:44 +0900

    Feat: "충돌 확인"

--- backend/src/main/java/a609/backend/controller/KakaoLoginController.java
+++ backend/src/main/java/a609/backend/controller/KakaoLoginController.java
@@ -2,8 +2,10 @@ package a609.backend.controller;
 
 import a609.backend.service.KakaoLoginService;
 import a609.backend.util.KaKaoUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,6 +13,9 @@ import javax.servlet.http.HttpSession;
 import java.util.HashMap;
 import java.util.Map;
 
+
+
+@Slf4j
 @RestController
 @RequestMapping("/api/oauth/kakao")
 public class KakaoLoginController {
@@ -21,10 +26,13 @@ public class KakaoLoginController {
     @Autowired
     KakaoLoginService kakaoLoginService;
 
-    @RequestMapping("/login")
+    @PostMapping("/login")
     public ResponseEntity<?> login(@RequestParam("code") String code) {
         Map<String, Object> resultMap = new HashMap<>();
         HttpStatus status = null;
+        log.info("log info start");
+        log.debug("log debug start");
+
 
         String loginResult = kakaoLoginService.login(code);
         if(loginResult.equals("fail")) {
@@ -32,16 +40,23 @@ public class KakaoLoginController {
             resultMap.put("message", "존재하지 않는 계정입니다.");
         }else {
             status = HttpStatus.OK;
-            resultMap.put("token", loginResult);
+            resultMap.put("사용자 정보", loginResult);//차후수정
         }
         return new ResponseEntity<Map<String, Object>>(resultMap, status);
+        //필요하면 claim에 담아서 보내줘야함
     }
 
-    @RequestMapping(value="/logout")
-    public ResponseEntity<?> logout(HttpSession session) {
-        kaKaoUtil.kakaoLogout((String)session.getAttribute("accessToken"));
-        session.removeAttribute("accessToken");
-        session.removeAttribute("userId");
+    @GetMapping("/logout")
+    public ResponseEntity<?> logout() {
+        kakaoLoginService.logout();
         return new ResponseEntity<String>("success", HttpStatus.OK);
     }
+
+    @DeleteMapping("/delete/{rt}")
+    public ResponseEntity<?> deletUser(@PathVariable String rt) {
+//        String loginResult = kakaoLoginService.deleteUser();
+       String newToken=kaKaoUtil.updateAccessToken(rt);
+        return new ResponseEntity<String>(newToken, HttpStatus.OK);
+    }
+
 }
