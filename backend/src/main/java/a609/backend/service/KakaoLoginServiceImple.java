aa7b3db80f6386067b0ce8836e8a401834c4fa06
양소정 <thwjd793@naver.com>
2022-07-22 13:59:07 +0900

    Feat: "카카오 로그아웃/회원탈퇴" #S07P12A609-30 #S07P12A609-29

--- backend/src/main/java/a609/backend/service/KakaoLoginServiceImple.java
+++ backend/src/main/java/a609/backend/service/KakaoLoginServiceImple.java
@@ -1,14 +1,13 @@
 package a609.backend.service;
 
-import a609.backend.db.entity.User;
 import a609.backend.util.JwtUtil;
 import a609.backend.util.KaKaoUtil;
-import io.jsonwebtoken.Jwt;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.HashMap;
 
+@Slf4j
 @Service
 public class KakaoLoginServiceImple implements KakaoLoginService {
 
@@ -16,7 +15,7 @@ public class KakaoLoginServiceImple implements KakaoLoginService {
     KaKaoUtil kaKaoUtil;
 
     @Autowired
-    JwtUtil jwtUtil;
+    UserService userService;
 
     @Override
     public String login(String code) {
@@ -25,19 +24,42 @@ public class KakaoLoginServiceImple implements KakaoLoginService {
 
         // 2번 인증코드로 토큰 전달
         HashMap<String, Object> userInfo = kaKaoUtil.getUserInfo(accessToken);
-        //3. 카카오 id로 회원가입 처리
-        //4. 강제 로그인처리
-        //5. response Header에 JWT토큰 추가
+
+        //3. 카카오 id로 DB에 회원가입 처리
+        //4. response Header에 JWT토큰 추가
+        //인코딩해서 DB저장하기
 
         System.out.println("login info : " + userInfo.toString());
+        log.info(userInfo.toString());
 
 
         if(userInfo.get("email") != null) {
 
-            String token = jwtUtil.createToken((String) userInfo.get("email"),1, (String) userInfo.get("name"),true);
+//          String token = jwtUtil.generateJwtToken();
+            String token = userInfo.get("nickname").toString();//차후 수정
             return token;
         }else {
             return "fail";
         }
+    }
+
+    @Override
+    public String logout() {
+        String refreshToken = "/DB에서 가져오기";
+        String accessToken = kaKaoUtil.updateAccessToken(refreshToken);
+        kaKaoUtil.kakaoLogout(accessToken);
+
+        return null;
+    }
+
+
+    @Override
+    public void deleteUser() {
+        String refreshToken = "/DB에서 가져오기";
+        String accessToken = kaKaoUtil.updateAccessToken(refreshToken);
+        kaKaoUtil.unlink(accessToken);
+//        DB에서 삭제
+//        userService.deleteUser("id");
+
     }
 }
