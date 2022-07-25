3574bf991648eb950867b69d72035730bf55f383
이진오 <efylo@naver.com>
2022-07-25 14:08:11 +0900

    Feat: "여행 일정 타이틀 컴포넌트 추가" #S07P12A609-134

--- frontend/src/routes/Travel.js
+++ frontend/src/routes/Travel.js
@@ -1,10 +1,15 @@
 import Header from "components/Header"
+import TravelTitle from "components/Travel/TravelTitle";
 import React from "react";
 
 function Travel() {
     return (
         <React.Fragment>
-            <Header />
+            <Header>
+                <p style={{"fontSize": "10px"}}>여행 계획 기록</p>
+            </Header>
+            <TravelTitle />
+            <hr width="80%" />
         </React.Fragment>
     )
 }
