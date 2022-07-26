acbce572f731ad07b79ef529c9d463ac0a43cbf4
이진오 <efylo@naver.com>
2022-07-26 10:36:36 +0900

    Feat: "여행 일정 관리 페이지 구현" #S07P12A609-168

--- frontend/src/components/Travel/TravelTitle.js
+++ frontend/src/components/Travel/TravelTitle.js
@@ -3,36 +3,43 @@ import "globalStyle.css"
 import { ReactComponent as User } from 'assets/user.svg'
 import React from "react";
 
-function TravelTitle({ title, startDate, profileURL, styles, members, joinLink, budget }) {
+function TravelTitle({ Dummy }) {
+    const { title, startDate, periodInDays, profileURL, styles, members, joinAddress, budget } = Dummy;
 
     return (
         <div className="container-title">
             <div className="travel-profile">
                 <img
                     width={"40"}
-                    src="./icons/plane.png"
+                    src={profileURL}
                     alt="여행 프로필 이미지"
                 />
             </div>
             <div className="travel-title">
-                여행 제목
+                {title}
             </div>
             <div className="travel-edit">
                 <a href="./">편집</a>
             </div>
             <div className="travel-date">
-                22.07.14 ~ 22.07.17
+                {startDate} ({periodInDays})
             </div>
             <div className="travel-info">
-                총 예산: 100만원<br />
-                #액티비티 #식도락 <a href="./">더보기</a>
+                총 예산: {budget}<br />
+                {
+                    styles.map((style, i) => 
+                        <div className="inline" key={i}>#{style}</div>
+                    )
+                }
+                <div className="inline"> </div>
+                <a href="./">더보기</a>
             </div>
             <div className="travel-emoji">
-                <User width={20} height={20} display="inline" />3
+                <User width={20} height={20} display="inline" />{members.length}
                 <img
                     width={24}
                     src="https://developers.kakao.com/assets/img/about/logos/kakaotalksharing/kakaotalk_sharing_btn_medium.png"
-                    alt="카카오톡 공유 보내기 버튼"
+                    alt={"카카오톡 공유 보내기 버튼" + joinAddress}
                 />
             </div>
         </div>
