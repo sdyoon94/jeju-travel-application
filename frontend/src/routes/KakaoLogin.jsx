fc8049561305583af36747dbe0c1b7561cc4596a
j1ddong <hju191228@gmail.com>
2022-07-28 10:16:40 +0900

    Fix: "api키 변경"

--- frontend/src/routes/KakaoLogin.jsx
+++ frontend/src/routes/KakaoLogin.jsx
@@ -1,7 +1,7 @@
 import "./Login.css"
 
 const REDIRECT_URI = "http://localhost:3000/kakao/callback"
-const CLIENT_ID = process.env.REACT_APP_CLIENT_ID
+const CLIENT_ID = "55d1ecfd6dece29b03e7b101cd3a0753"
 const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}`
 
 // console.log(window.location.href)
