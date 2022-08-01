5e1aa463ee537dfc3b5b1bfa935e40211068c6c5
김창현 <ckdgus8753@gmail.com>
2022-08-01 15:49:49 +0900

    Rename: "여행입력라우트분리"

--- frontend/src/routes/Inputs/InputMemberCnt.jsx
+++ frontend/src/routes/Inputs/InputMemberCnt.jsx
@@ -9,7 +9,9 @@ import Departure from "components/Inputs/Departure";
 import Style from "components/Inputs/Style";
 // import Budget from "components/Inputs/Budget"
 // import StartTime from "components/Inputs/StartTime"
-import Calender from "components/Inputs/Calender";
+import Calender from "components/Inputs/Calender"
+
+
 
 function Inputs() {
   // const [checked, setChecked] = React.useState(false);
@@ -25,13 +27,13 @@ function Inputs() {
   return (
     <div className="input-container">
       <div className="input-content">
-        {/* <Departure></Departure> */}
+        <Departure></Departure>
         {/* <Duration></Duration> */}
         {/* <Style></Style> */}
         {/* <Budget></Budget> */}
         {/* <StartTime></StartTime> */}
         {/* <Style></Style> */}
-        <Calender></Calender>
+        {/* <Calender></Calender> */}
       </div>
       <div className="input-footer">
         <Button className="input-btn" variant="contained">
