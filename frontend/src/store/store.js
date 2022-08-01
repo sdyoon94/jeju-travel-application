5d13173d93d6984b68bddc6c9e970f3e9fe60bfe
이진오 <efylo@naver.com>
2022-08-01 13:14:22 +0900

    Chore: "Redux store - 여행, 경로 관련 설정 추가"

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,12 +1,21 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit"
 import tripList from './modules/tripListSlice'
+import travel from './modules/travelSlice'
+import distanceMatrix from './modules/distanceMatrixSlice'
+import distance from './modules/distanceSlice'
 
 const rootReducer = combineReducers({
-    tripList
+    tripList,
+    travel,
+    distanceMatrix,
+    distance
 })
 
 
 
 export default configureStore({
     reducer: rootReducer,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+        serializableCheck: false
+    })
 })
