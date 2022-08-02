89aeafa7eaed0d502fada0a2ac23cbf2baa802cf
김지건 <jigun21@naver.com>
2022-08-02 20:09:42 +0900

    fix : "버그 수정 및 usertrip 로직 수정"

---
+++ backend/src/main/java/a609/backend/db/repository/UserTripRepository.java
@@ -0,0 +1,18 @@
+package a609.backend.db.repository;
+
+import a609.backend.db.entity.Trip;
+import a609.backend.db.entity.User;
+import a609.backend.db.entity.UserTrip;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface UserTripRepository extends JpaRepository<UserTrip, Long> {
+
+    UserTrip save(UserTrip userTrip);
+
+    List<UserTrip> findByUserKakaoId(String kakaoId);
+
+    List<UserTrip> findByTripTripId(int tripId);
+
+}
