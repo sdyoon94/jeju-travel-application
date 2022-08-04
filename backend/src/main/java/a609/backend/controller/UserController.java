f3fec1293b707c41c79872e46fdf47993bff7120
sdyoon94 <sdyoon94@gmail.com>
2022-08-04 13:53:09 +0900

    Chore: "CORS 해결 테스트"

--- backend/src/main/java/a609/backend/controller/UserController.java
+++ backend/src/main/java/a609/backend/controller/UserController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.HashMap;
 import java.util.Map;
 
-@CrossOrigin("*")
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1")
 public class UserController {
