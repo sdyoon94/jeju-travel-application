b07234ce977a98ea8bec563e8368e386825a6f13
김지건 <jigun21@naver.com>
2022-08-05 13:19:32 +0900

    feat: "검색 기능 구현 및 db테스트" #S07P12A609-266

--- backend/src/main/java/a609/backend/controller/PlaceController.java
+++ backend/src/main/java/a609/backend/controller/PlaceController.java
@@ -7,24 +7,21 @@ import a609.backend.service.PlaceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/place")
+@RequestMapping("/api/v1/place")
 public class PlaceController {
 
     @Autowired
     PlaceService placeService;
 
-    @GetMapping("/find/{keyword}")
-    public ResponseEntity<?> findPlace(@PathVariable String keyword){
+    @GetMapping("/find")
+    public ResponseEntity<?> findPlace(@RequestParam("q") String keyword){
         Map<String, Object> resultMap = new HashMap<>();
         List<FindPlaceDTO> findPlaces = placeService.findPlace(keyword);
         resultMap.put("findPlaces", findPlaces);
