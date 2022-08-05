2fa2bf1a55575a81f0ddfe6349a68b846678da39
이진오 <efylo@naver.com>
2022-08-05 14:02:37 +0900

    Feat: "여행 참여 페이지 구현(비동기 로직 제외)" #S07P12A609-260

---
+++ frontend/src/routes/TravelJoin.jsx
@@ -0,0 +1,101 @@
+import { CircularProgress } from "@mui/material";
+import { useState } from "react"
+import { useSelector } from "react-redux"
+import "./TravelJoin.css"
+import "../globalStyle.css"
+
+const { useParams, useNavigate } = require("react-router-dom");
+
+const URL = "http://i7a609.p.ssafy.io:8081/oauth2/authorization/kakao"
+
+function TravelJoin() {
+
+  const navigate = useNavigate()
+  const { travelUid } = useParams()
+  const { id } = useSelector((state) => state.auth)
+
+  const isLoggedIn = id ? true : false
+
+  const [ title, setTitle ] = useState("여행 제목")
+  
+  /* 비동기 통신 - 여행 메타 정보 받아오는 부분
+
+  const [ isLoaded, setIsLoaded ] = useState(false)
+  try {
+    const resp = axios({
+      method: "get",
+      url: `~/join/${travelUid}/meta`
+    })
+    setTitle(resp.data.title)
+    setIsLoaded(true)
+  }
+  catch (err) {
+    alert("여행 메타 정보를 불러오는 데 문제가 발생하였습니다. ")
+    navigate("/", {
+      replace: true
+    })
+  }
+   */ 
+
+  // 홈으로 가기 클릭 시 동작
+  const handleHomeClick = () => {
+    navigate("/", {
+      replace: false
+    })
+  }
+
+  // 참여하기 클릭 시 동작
+  const handleJoinClick = async () => {
+    /* 
+    try {
+      const resp = await axios({
+        method: "get",
+        url: `~/join/${travelUid}`
+      })
+      // 메인 페이지로 보내거나, 바로 여행에 참여시키거나
+      navigate("/", {
+        replace: true
+      })
+    }
+    catch (err) {
+      alert("현재 여행 참여가 어려운 상황입니다. ")
+    }
+    */
+  }
+
+  return (
+    <div className="travel-join-container">
+      {/* { isLoaded ? 
+        <div className="travel-join-title">
+          { title }에 초대되었습니다. <br />
+          여행에 참여하시겠어요?
+        </div> :
+        <CircularProgress />
+      } */}
+      <div className="travel-join-title title-size">
+        { title }에 초대되었습니다. <br />
+        여행에 참여하시겠어요?
+      </div>
+      <div className="travel-join-body">
+        { isLoggedIn ? 
+          <div onClick={handleJoinClick} className="travel-join-btn">
+            참여하기
+          </div> 
+          : 
+          <>
+            <p>로그인 후 이용할 수 있는 서비스입니다. </p>
+            <a href={URL}>
+              <img className="kakao-box" alt="kakaoLoginBtn" src="/icons/kakaoLogo.png"></img>
+            </a>
+          </>
+        }
+        <p onClick={handleHomeClick} className="home-link">
+          홈으로 가기
+        </p>
+      </div>
+    </div>
+  )
+
+}
+
+export default TravelJoin
