940016ba6f557e09ac1aaa19d6565679d32ba7cd
이진오 <efylo@naver.com>
2022-08-08 16:09:07 +0900

    Feature: "여행 정보(info), 스케쥴(schedule) API 서버 연결" #S07P12A609-294

--- frontend/src/store/modules/travelSlice.js
+++ frontend/src/store/modules/travelSlice.js
@@ -1,44 +1,88 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
 import axios from "axios"
-import travel from "dummies/DummyTravel.json"
+// import travel from "dummies/DummyTravel.json"
 
 // TODO: createAsyncThunk
 const HOST = "http://i7a609.p.ssafy.io:8081/api/v1/"
 const SCHEDULE_PATH = "schedule"
+const TRAVEL_PATH = "travel/showTripInfo"
 
-const buildTripSchedulePath = (tripId, day) => `${HOST + SCHEDULE_PATH}?tripId=${tripId}&day=${day}`
-
-const buildTripScheduleConfig = (tripId) => ({
+const buildScheduleConfig = (travelId, day) => ({
 	method: "get",
-	url: buildTripSchedulePath(tripId)
+	url: `${HOST + SCHEDULE_PATH}?tripId=${travelId}&day=${day}`
+})
+
+const buildTravelConfig = (travelId) => ({
+	method: "get",
+	url: `${HOST + TRAVEL_PATH}/${travelId}`
 })
 
 export const fetchTravel = createAsyncThunk(
-	"trip/schedule/tripId",
-	async ({ tripId }, thunkAPI) => {
+	"travel",
+	async ({ travelId }, thunkAPI) => {
+		const dispatch = thunkAPI.dispatch
+
 		try {
-			const response = await axios(buildTripScheduleConfig(tripId))
+			const response = await axios(buildTravelConfig(travelId))
+			console.log(response)
 
-			console.log(response.data)
+			if (response.status === 200) {
+				const info = response.data.tripInfo
+				console.log(info)
 
-			return response.data
+				dispatch(setTravelInfo({ payload: info }))
+				
+				const { periodInDays } = info
+				dispatch(initSchedule({ payload: periodInDays }))
+
+				for (let day = 0; day < periodInDays; day++) {
+					const response = await axios(buildScheduleConfig(travelId, day))
+					console.log(response)
+
+					if (response.status === 200) {
+						const schedule = response.data["일자별 Schedule List"]
+						console.log(schedule)
+
+						dispatch(setSchedule({ day, schedule }))
+					}
+				}
+			}
 		}
 		catch (err) {
-			if (err.response) {
-				return thunkAPI.rejectWithValue(`fetchTravel response error - ${err.response.status}`)
-			}
-			else if (err.request) {
-				return thunkAPI.rejectWithValue(`fetchTravel request error`)
-			}
-			else {
-				return thunkAPI.rejectWithValue(`${err.message}`)
-			}
+			console.log(err)
+			return
 		}
 	}
 )
 
+/* 
+info: {
+	tripId: 여행 아이디,
+	tripName: 여행 이름, 
+	startDate: 시작 일자 (YYYY-MM-DD),
+	periodInDays: 여행 일 수,
+	budget: 여행 예산,
+	vehicle: 주 교통 수단 (car, walk),
+	style: 여행 스타일 (bit-masking),
+	member: 	멤버 정보 배열
+	  - kakaoId,
+		- nickname,
+		- imagePath,
+}
+ */
+
 const initialState = {
-	...travel,
+	info: {
+		tripId: 0,
+		tripName: "",
+		startDate: "",
+		periodInDays: 0,
+		budget: 0,
+		vehicle: "",
+		style: 0,
+		member: []
+	},
+	schedules: [],
 	error: null
 }
 
@@ -55,6 +99,30 @@ const travelSlice = createSlice({
 		setTravel(state, { payload }) {
 			state[payload[0]] = payload[1]
 		},
+		setTravelInfo(state, { payload: info }) {
+			state = {
+				...state,
+				info
+			}
+		},
+		initSchedule(state, { payload: periodInDays }) {
+			state = {
+				...state,
+				schedules: new Array(periodInDays)
+			}
+		},
+		setSchedule(state, { payload: { day, schedule } }) {
+			const schedules = state.schedules
+			schedules[day] = {
+				day: day + 1,
+				schedule
+			}
+
+			state = {
+				...state,
+				schedules
+			}
+		}
 	},
 	extraReducers: (builder) => {
 		builder
@@ -70,5 +138,5 @@ const travelSlice = createSlice({
 
 const { actions, reducer } = travelSlice
 
-export const { editStartTime, editStayTime, setTravel } = actions
+export const { editStartTime, editStayTime, setTravel, setTravelInfo, initSchedule, setSchedule } = actions
 export default reducer
