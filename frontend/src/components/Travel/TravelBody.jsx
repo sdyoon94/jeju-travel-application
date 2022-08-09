72cffaf65c2c913598699e843a7aea3e3cd9a7e0
이진오 <efylo@naver.com>
2022-08-09 23:55:12 +0900

    Refactor: "사용하지 않는 컴포넌트, 함수 제거"

--- frontend/src/components/Travel/TravelBody.jsx
+++ frontend/src/components/Travel/TravelBody.jsx
@@ -1,8 +1,5 @@
 import { useEffect, useState } from "react"
-import { useSelector } from "react-redux"
 import { format, addDays } from "date-fns"
-
-import { getAllDates } from "components/DateTime/date"
 
 import Schedule from "./Body/Schedule"
 import Day from "./Body/Day"
