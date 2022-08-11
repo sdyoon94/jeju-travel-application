7ad7103ce7b54328251c804191d8363d2d48a46f
양소정 <thwjd793@naver.com>
2022-08-11 22:47:29 +0900

    Revert "Merge branch 'feature-back/algo' into 'master'"
    This reverts merge request !127

---
+++ frontend/src/component/Signup.js
@@ -0,0 +1,7 @@
+function Signup() {
+  return (
+    <h1>여기는 회원가입</h1>
+  )
+}
+
+export default Signup
--- frontend/src/setupProxy.js
+++
@@ -1,8 +0,0 @@
-const {createProxyMiddleware}=require('http-proxy-middleware');
-
-module.exports=function(app){
-  createProxyMiddleware('/api',{
-    target: 'http://i7a609.p.ssafy.io:8081',
-    changeOrigin: true
-  })
-}
