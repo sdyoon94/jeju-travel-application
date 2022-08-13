eb21797d85cbc370fb2e5f0fe225c7fc67340146
김지건 <jigun21@naver.com>
2022-08-13 16:04:57 +0900

    fix: "중복 참여문제 수정"

--- backend/src/main/java/a609/backend/service/TripService.java
+++ backend/src/main/java/a609/backend/service/TripService.java
@@ -1,11 +1,8 @@
 package a609.backend.service;
 
 import a609.backend.db.entity.Trip;
-import a609.backend.db.entity.User;
 import a609.backend.payload.response.FindTripDTO;
 import a609.backend.payload.response.TripInfoDTO;
-
-import java.util.List;
 
 public interface TripService {
 
@@ -15,7 +12,7 @@ public interface TripService {
     String registerTrip(Trip trip,String token);
     void updateTrip(Long tripId,Trip trip);
     void deleteTrip(Long tripId);
-    void addUser(Long tripId, String token);
+    boolean addUser(Long tripId, String token);
     void deleteUserTrip(Long tripId, String token);
 
 }
