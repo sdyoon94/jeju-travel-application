a135b6d27bfa079ba488a5dbcc2497d46b8f121b
sdyoon94 <sdyoon94@gmail.com>
2022-08-15 00:28:45 +0900

    Fix: "이미지 저장 수정"

--- backend/src/main/java/a609/backend/service/FileServiceImpl.java
+++ backend/src/main/java/a609/backend/service/FileServiceImpl.java
@@ -7,7 +7,6 @@ import com.drew.imaging.ImageMetadataReader;
 import com.drew.metadata.Directory;
 import com.drew.metadata.Metadata;
 import com.drew.metadata.exif.ExifIFD0Directory;
-import com.drew.metadata.jpeg.JpegDirectory;
 import lombok.extern.slf4j.Slf4j;
 import org.imgscalr.Scalr;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -39,14 +38,14 @@ public class FileServiceImpl implements FileService {
         Long id = ((Long) jwtUtil.parseJwtToken(token).get("id"));
 
 
-        String uploadPath = File.separator + "profile_images";
+        String uploadPath = File.separator + "profile_images" + File.separator + id;
 //        String dbPath = File.separator + "saimedia" + File.separator + "Album";
 
         try {
             // 실행되는 위치의 'files' 폴더에 파일이 저장됩니다.
 //            String savePath = System.getProperty("user.dir") + "\\files";
-//            String savePath = uploadPath+File.separator+id+File.separator+id + "." + extractExt(file.getOriginalFilename());
             String savePath = uploadPath + File.separator + id + "." + extractExt(file.getOriginalFilename());
+//            String savePath = uploadPath + File.separator + id + "." + extractExt(file.getOriginalFilename());
 //            String TestSavePath = uploadPath + File.separator + id + File.separator + id + "." + extractExt(file.getOriginalFilename());
 
 
@@ -62,10 +61,11 @@ public class FileServiceImpl implements FileService {
 //            if (userRepository.findOneByKakaoId(id).getDbImagePath() != null) {
 //                this.deleteById(token);
 //            }
-            File check = new File(savePath);
-            if(check.exists()){
+            File check = new File(uploadPath);
+            if (check.exists()) {
                 check.delete();
             }
+            check.mkdir();
             Path path = Paths.get(savePath);
             file.transferTo(path);
 
@@ -127,43 +127,43 @@ public class FileServiceImpl implements FileService {
         int rotate = 1;
 
 
-            // 이미지 품질 설정
+        // 이미지 품질 설정
 // Image.SCALE_DEFAULT : 기본 이미지 스케일링 알고리즘 사용
 // Image.SCALE_FAST : 이미지 부드러움보다 속도 우선
 // Image.SCALE_REPLICATE : ReplicateScaleFilter 클래스로 구체화 된 이미지 크기 조절 알고리즘
 // Image.SCALE_SMOOTH : 속도보다 이미지 부드러움을 우선
 // Image.SCALE_AREA_AVERAGING : 평균 알고리즘 사용
 
-            //회전후 저장 위해 메타데이터 읽기
-            try{
-                Metadata metadata = ImageMetadataReader.readMetadata(file.getInputStream());
-                Directory directory = metadata.getFirstDirectoryOfType(ExifIFD0Directory.class);
-                if(directory!=null){
-                    rotate = directory.getInt(ExifIFD0Directory.TAG_ORIENTATION);
-                }
-            } catch(Exception e){
-                System.out.println("메타데이터 읽기에서 에러났다아아아");
-                e.printStackTrace();
+        //회전후 저장 위해 메타데이터 읽기
+        try {
+            Metadata metadata = ImageMetadataReader.readMetadata(file.getInputStream());
+            Directory directory = metadata.getFirstDirectoryOfType(ExifIFD0Directory.class);
+            if (directory != null) {
+                rotate = directory.getInt(ExifIFD0Directory.TAG_ORIENTATION);
             }
-            switch(rotate){
-                case 6:
-                    inputImage = Scalr.rotate(inputImage, Scalr.Rotation.CW_90);
-                    break;
-                case 3:
-                    inputImage = Scalr.rotate(inputImage, Scalr.Rotation.CW_180);
-                    break;
-                case 8:
-                    inputImage = Scalr.rotate(inputImage, Scalr.Rotation.CW_270);
-            }
-            int originWidth = inputImage.getWidth();
-            int originHeight = inputImage.getHeight();
-            // 변경할 가로 길이
-            int newWidth = 500;
-            if (originWidth > newWidth) {
+        } catch (Exception e) {
+            System.out.println("메타데이터 읽기에서 에러났다아아아");
+            e.printStackTrace();
+        }
+        switch (rotate) {
+            case 6:
+                inputImage = Scalr.rotate(inputImage, Scalr.Rotation.CW_90);
+                break;
+            case 3:
+                inputImage = Scalr.rotate(inputImage, Scalr.Rotation.CW_180);
+                break;
+            case 8:
+                inputImage = Scalr.rotate(inputImage, Scalr.Rotation.CW_270);
+        }
+        int originWidth = inputImage.getWidth();
+        int originHeight = inputImage.getHeight();
+        // 변경할 가로 길이
+        int newWidth = 500;
+        if (originWidth > newWidth) {
             // 기존 이미지 비율을 유지하여 세로 길이 설정
             int newHeight = (originHeight * newWidth) / originWidth;
 
-           Image resizeImage = inputImage.getScaledInstance(newWidth, newHeight, Image.SCALE_FAST);
+            Image resizeImage = inputImage.getScaledInstance(newWidth, newHeight, Image.SCALE_FAST);
             BufferedImage newImage = new BufferedImage(newWidth, newHeight, BufferedImage.TYPE_INT_RGB);
             Graphics graphics = newImage.getGraphics();
             graphics.drawImage(resizeImage, 0, 0, null);
