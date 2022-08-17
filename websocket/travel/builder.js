870cc015f188b7be1c2a1b6ce1fad51a3d954fc5
이진오 <efylo@naver.com>
2022-08-17 15:40:30 +0900

    Fix: "revoke 오타 수정" #

--- websocket/travel/builder.js
+++ websocket/travel/builder.js
@@ -112,8 +112,6 @@ const travelBuilder = (io, nsp) => {
 
       revokeAllAuthorities(travelId, roomTable, { id })
 
-      revokeAllAuthorities(travelId, roomTable, { id: socket.data.id })
-
       if (travelId && popSocket(socket, travelId, roomTable) === 0) {
         try {
           await updateTravelInfo(travelId, roomTable, token)
